0219bebc503f91e75c9ca1728d92b23eb3890ead
yosa-odoo <yosa@odoo.com>
2024-11-09 14:22:01 +0100

    day 04 gold

--- 2023/day_4/query_2.sql
+++ 2023/day_4/query_2.sql
@@ -1,3 +1,67 @@
 \include_relative 'common.sql'
 
-select * from day04.points_per_card order by card;
+WITH RECURSIVE recursively_won_cards AS (
+    SELECT
+        card,
+        points,
+        1 AS copies
+    FROM day04.points_per_card
+
+    UNION ALL
+
+    SELECT
+        c.card + i AS card,
+        m.points,
+        c.copies AS copies
+    FROM recursively_won_cards AS c
+    CROSS JOIN generate_series(1, c.points) AS i
+    JOIN day04.points_per_card AS m
+        ON c.card + i = m.card
+    WHERE c.points > 0
+)
+
+SELECT
+    SUM(copies) AS total_copies
+FROM recursively_won_cards
+;
+
+--
+-- Our initial non-recursive term will be
+-- card | points | copies
+-- ------+--------+--------
+--     1 |      4 |      1      -> iteration 1
+--     2 |      2 |      1
+--     3 |      2 |      1
+--     4 |      1 |      1
+--     5 |      0 |      1
+--     6 |      0 |      1
+--
+-- ITERATION 1
+-- select * from (VALUES (1, 4, 1)) as t(card,points,copies);
+--  cte: recursively_won_cards
+--  card | points | copies
+-- ------+--------+--------
+--     1 |      4 |      1
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
